64d1dd7ced5ad3c546121707e214d650e6bcc113
Anshu Naagar <anshunaagar.academics@gmail.com>
2026-06-25 14:14:32 +0530

    Raw-CMAKE codebase (init)

---
+++ cppcmathquant.cpp
@@ -0,0 +1,12 @@
+﻿// cppcmathquant.cpp : Defines the entry point for the application.
+//
+
+#include "cppcmathquant.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
