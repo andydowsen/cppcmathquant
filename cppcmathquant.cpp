376df7072a8d32ed3d97441dcd17cefd2f63c49d
Anshu Naagar <andydowsen47us@gmail.com>
2026-06-25 14:32:26 +0530

    Test-winapi

--- cppcmathquant.cpp
+++ cppcmathquant.cpp
@@ -1,6 +1,41 @@
-﻿#include "cppcmathquant.h"
-using namespace std;
+﻿#include <windows.h>
+#include <sapi.h>
+#include <iostream>
 
 int main(int argc, const char *argv[]) {
-	return argc;
+    HRESULT hr = CoInitialize(nullptr);
+
+    if (FAILED(hr))
+    {
+        std::cerr << "COM initialization failed\n";
+        return 1;
+    }
+
+    ISpVoice* pVoice = nullptr;
+
+    hr = CoCreateInstance(
+        CLSID_SpVoice,
+        nullptr,
+        CLSCTX_ALL,
+        IID_ISpVoice,
+        (void**)&pVoice
+    );
+
+    if (SUCCEEDED(hr))
+    {
+        pVoice->Speak(
+            L"Hello. Windows Speech API is working correctly.",
+            SPF_DEFAULT,
+            nullptr
+        );
+
+        pVoice->Release();
+    }
+    else
+    {
+        std::cerr << "Failed to create voice instance\n";
+    }
+
+    CoUninitialize();
+    return 0;
 }
