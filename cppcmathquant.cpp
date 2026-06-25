3e3f12261840ea12918710c20359b3e7691dd03c
Anshu Naagar <anshunaagar.academics@gmail.com>
2026-06-25 14:19:56 +0530

    Test-commit(x-64dbgcmake)

--- cppcmathquant.cpp
+++ cppcmathquant.cpp
@@ -1,12 +1,6 @@
-﻿// cppcmathquant.cpp : Defines the entry point for the application.
-//
-
-#include "cppcmathquant.h"
-
+﻿#include "cppcmathquant.h"
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+int main(int argc, const char *argv[]) {
+	return argc;
 }
